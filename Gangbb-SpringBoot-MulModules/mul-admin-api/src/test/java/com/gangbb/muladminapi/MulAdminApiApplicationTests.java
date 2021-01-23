2f79b1b5b29b3a34cb00105c395bae9a8783faff
Gangbb <949526365@qq.com>
2021-01-23 23:28:35 +0800

    Gangbb-SpringBoot-MulModules

---
+++ Gangbb-SpringBoot-MulModules/mul-admin-api/src/test/java/com/gangbb/muladminapi/MulAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangbb.muladminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Gangbb-SpringBoot-MulModules/mul-utils/src/test/java/com/gangbb/mulutils/MulUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangbb.mulutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
