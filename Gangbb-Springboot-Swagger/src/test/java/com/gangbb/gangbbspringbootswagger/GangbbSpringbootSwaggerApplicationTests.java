6b21106879d2b420b18209fd227f87e7d8079c4d
Gangbb <949526365@qq.com>
2021-01-24 15:29:14 +0800

    add swagger

---
+++ Gangbb-Springboot-Swagger/src/test/java/com/gangbb/gangbbspringbootswagger/GangbbSpringbootSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangbb.gangbbspringbootswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangbbSpringbootSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
