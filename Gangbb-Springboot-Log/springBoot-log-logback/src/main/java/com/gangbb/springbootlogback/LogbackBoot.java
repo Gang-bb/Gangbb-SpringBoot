1e8b8875c2b94ec515a46c36888f08215be70f50
liangyixiang <949526365@qq.com>
2021-12-12 08:29:37 +0800

    feat:logback测试代码上传

---
+++ Gangbb-Springboot-Log/springBoot-log-logback/src/main/java/com/gangbb/springbootlogback/LogbackBoot.java
@@ -0,0 +1,18 @@
+package com.gangbb.springbootlogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Springboot logback实践启动类
+ *
+ * @author Liangyixiang
+ * @date 2021/12/11
+ **/
+@SpringBootApplication
+public class LogbackBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackBoot.class, args);
+    }
+}
